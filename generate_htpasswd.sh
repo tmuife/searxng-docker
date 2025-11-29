4d5a5adf81407253c209b4fd599e16ae466c013b
jinsh <43266626@qq.com>
2025-11-29 22:47:50 +0800

    change to nginx

--- generate_htpasswd.sh
+++ generate_htpasswd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Generate htpasswd file for nginx basic auth
 apk update && apk add openssl
-echo "${user_name}:$(openssl passwd -5 '${password}')" > /etc/nginx/conf.d/search.htpasswd
+echo "${user_name}:$(openssl passwd -5 "${password}")" > /etc/nginx/conf.d/search.htpasswd
 # Start nginx
 exec nginx -g 'daemon off;'
